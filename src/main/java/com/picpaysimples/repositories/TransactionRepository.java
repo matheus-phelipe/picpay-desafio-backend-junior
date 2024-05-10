cbd334a2f8b68e29ccae2b35545ca0d785742cf3
Matheus Phelipe de Oliveira <matheusphetrabalho@gmail.com>
2024-05-10 15:06:31 -0300

    feat: create transaction repository

---
+++ src/main/java/com/picpaysimples/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.picpaysimples.repositories;
+
+import com.picpaysimples.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
